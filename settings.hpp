c60ff3ff39037f9182caaba7942cfd11f6e391d3
Sohang Chopra <31966963+sohang3112@users.noreply.github.com>
2017-09-22 19:28:45 +0530

    Update settings.hpp

--- settings.hpp
+++ settings.hpp
@@ -2,3 +2,4 @@
 #define FILE_CELL_SEPERATOR '\r'                //used in data file to indicate end of cell
                                                 //file will store data in binary format, 
                                                 //so we just need to use a character that cannot be used in a normal string
+#define MAX_COLUMNS 5             //Since we are printing to console, we cannot have an unlimited number of columns
