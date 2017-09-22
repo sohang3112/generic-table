f2d6e0df243b24685c34e3aaab8ce06e447b8c93
Sohang Chopra <31966963+sohang3112@users.noreply.github.com>
2017-09-22 18:48:39 +0530

    Create settings.hpp

---
+++ settings.hpp
@@ -0,0 +1,4 @@
+#define USER_PROMPT "\n>>> "
+#define FILE_CELL_SEPERATOR '\r'                //used in data file to indicate end of cell
+                                                //file will store data in binary format, 
+                                                //so we just need to use a character that cannot be used in a normal string
