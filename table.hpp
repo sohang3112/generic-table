0ec6b52c809b21f5a37d7d83bb70907940a4e90a
Sohang Chopra <31966963+sohang3112@users.noreply.github.com>
2017-09-23 21:34:27 +0530

    Create table.hpp

---
+++ table.hpp
@@ -0,0 +1,23 @@
+/* Manage table through file handling
+ * File Format: <n_cols> <cell> <CELL_DELIM> <cell> <CELL_DELIM> <cell> <CELL_DELIM> ... <cell> <CELL_DELIM>
+ * First <n_cols> (integer) is read, then first <n_cols> cells are read to form header (title bar).
+ * The remaining records are read by reading <n_cols> cells at a time.
+ * (For reading a cell, getline() may be used)
+ */
+ 
+#ifndef TABLE_INC
+#define TABLE_INC
+
+#include "settings.hpp"
+
+//Example interface (parameters may be added later)
+class Table {
+    public:
+        void add_record();
+        void modify_record();
+        void delete_record();
+        void find_record();
+        void display();
+};
+
+#endif        //TABLE_INC
